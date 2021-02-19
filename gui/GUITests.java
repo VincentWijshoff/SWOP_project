88b3385c8f0e3472de7f15e2a6904d8a8f02197b
Jonas Vanmalcot <jonas.vanmalcot@student.kuleuven.be>
2021-02-19 09:51:10 +0100

    add comments on how to load html code from url in GUITests

--- gui/GUITests.java
+++ gui/GUITests.java
@@ -1,6 +1,13 @@
 package gui;
 
+import browsrhtml.BrowsrDocumentValidator;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
 
 
 class GUITests {
@@ -28,4 +35,18 @@ class GUITests {
 		assertFalse(testName, rectangle.isInRectangle(100, 150));
 		assertFalse(testName, rectangle.isInRectangle(9, 9));
 	}
+
+
+
+	//load html code from url: (copy paste from https://www.programcreek.com/java-api-examples/?class=java.net.URL&method=openStream)
+	/*
+	URL url = new URL("https://...")
+	InputStream is = url.openStream()
+	byte[] buf = new byte[1024];
+	StringBuffer sb = new StringBuffer();;
+		while(-1 != inputStream.read(buf)) {
+			sb.append(new String(buf));
+		}
+	 */
+	//sb contains entire html code
 }
