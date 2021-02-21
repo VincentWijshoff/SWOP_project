3b55647a917c7eab598b36340d96c004536a40cc
Toon Van Hove <toon.vanhove@student.kuleuven.be>
2021-02-21 16:30:31 +0100

    1st attempt at rendering HTML

--- gui/DocumentArea.java
+++ gui/DocumentArea.java
@@ -1,5 +1,9 @@
 package gui;
 
+import html.HtmlA;
+import html.HtmlElement;
+import html.HtmlTable;
+
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -26,6 +30,28 @@ public class DocumentArea {
 
     public void draw(GUIObject obj) {
         drawnObjects.add(obj);
+    }
+
+    public void renderHTML(HtmlElement element) {
+        renderHTML(element, relativeYPos);
+    }
+
+    public void renderHTML(HtmlElement element, int startY) {
+        if (element instanceof HtmlTable) {
+            HtmlTable table = ((HtmlTable) element);
+            ArrayList<HtmlElement> tableRows = table.getTableRows();
+
+            int currentY = startY;
+            for (HtmlElement row: tableRows) {
+                renderHTML(row, currentY);
+                currentY += row.getHeight();
+            }
+        }
+        else if (element instanceof HtmlA) {
+            HtmlA link = (HtmlA) element;
+            drawnObjects.add(new GUIString(link.getText(), 0, startY));
+            //TODO add hyperlink functionality: make GUILink instead of plain GUIString?
+        }
     }
 
 }
