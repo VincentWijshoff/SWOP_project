7e9f44c546487abefb1a8e83e2b125b6a004908b
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2021-03-10 10:55:52 +0100

    addedd final documentation

--- Browsr.java
+++ Browsr.java
@@ -4,6 +4,10 @@ import localDocuments.Docs;
 
 public class Browsr {
 
+    /**
+     * Start the browser
+     * @param args The main arguments
+     */
     public static void main(String[] args) {
         Window gui = new Window("CoolBrowser");
         java.awt.EventQueue.invokeLater(gui::show);
