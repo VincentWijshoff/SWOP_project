8e5fb091889dd3b10d8400668c22cd299f0887bd
Toon Van Hove <toon.vanhove@student.kuleuven.be>
2021-03-11 09:22:00 +0100

    Fix test annotation

--- Tests/BrowsrDocumentValidatorTest.java
+++ Tests/BrowsrDocumentValidatorTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 class BrowsrDocumentValidatorTest {
 
-	@org.junit.Test
+	@Test
 	void testWithString() {
 		BrowsrDocumentValidator.assertIsValidBrowsrDocument("""
 				<table>
