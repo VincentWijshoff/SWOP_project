05b2406347073941d7a5310b5ca10f2d11533227
Toon Van Hove <toonvanhove@hotmail.com>
2021-03-28 15:19:56 +0200

    Fix use-case test 🎉🥳

--- src/tests/UseCaseTest.java
+++ src/tests/UseCaseTest.java
@@ -23,7 +23,7 @@ public class UseCaseTest {
 
         //2. Application shows a welcome document.
         assertTrue("UC_2.a", window.getAddress().equals("WelcomeDoc.html"));
-        assertTrue("UC_2.b", window.getDocArea().getDrawnGUIObjects().size() == 7);
+        assertTrue("UC_2.b", window.getDocArea().getDrawnGUIObjects().size() == 8);
         //Welcome doc has 6 GUIStrings and 1 GUILink
 
         //3. User navigates to a desired webpage. (using AddressBar)
@@ -34,16 +34,16 @@ public class UseCaseTest {
         //4. Application shows the desired webpage.
         assertEquals("UC_4.a", window.getAddress(), "https://people.cs.kuleuven.be/~bart.jacobs/browsrtest.html");
         ArrayList<GUIObject> renderedObjects = window.getDocArea().getDrawnGUIObjects();
-        assertTrue("UC_4.b", renderedObjects.size() == 9);
-        assertTrue("UC_4.c", containsGUIStringWith(90, 98, "Tables", renderedObjects));
-        assertTrue("UC_4.d", containsGUILinkWith(10, 82, "a", "a.html", renderedObjects));
-        assertTrue("UC_4.e", containsGUILinkWith(10, 98, "table", "table.html", renderedObjects));
-        assertTrue("UC_4.f", containsGUIStringWith(90, 130, "Table cells containing table data", renderedObjects));
-        assertTrue("UC_4.g", containsGUIStringWith(90, 82, "Hyperlink anchors", renderedObjects));
-        assertTrue("UC_4.h", containsGUILinkWith(10, 130, "td", "td.html", renderedObjects));
-        assertTrue("UC_4.i", containsGUIStringWith(90, 114, "Table rows", renderedObjects));
-        assertTrue("UC_4.j", containsGUIStringWith(10, 66, "HTML elements partially supported by Browsr:", renderedObjects));
-        assertTrue("UC_4.h", containsGUILinkWith(10, 114, "tr", "tr.html", renderedObjects));
+        assertTrue("UC_4.b", renderedObjects.size() == 11);
+        assertTrue("UC_4.c", containsGUIStringWith(27, 32, "Tables", renderedObjects));
+        assertTrue("UC_4.d", containsGUILinkWith(0, 16, "a", "a.html", renderedObjects));
+        assertTrue("UC_4.e", containsGUILinkWith(0, 32, "table", "table.html", renderedObjects));
+        assertTrue("UC_4.f", containsGUIStringWith(27, 64, "Table cells containing table data", renderedObjects));
+        assertTrue("UC_4.g", containsGUIStringWith(27, 16, "Hyperlink anchors", renderedObjects));
+        assertTrue("UC_4.h", containsGUILinkWith(0, 64, "td", "td.html", renderedObjects));
+        assertTrue("UC_4.i", containsGUIStringWith(27, 48, "Table rows", renderedObjects));
+        assertTrue("UC_4.j", containsGUIStringWith(0, 0, "HTML elements partially supported by Browsr:", renderedObjects));
+        assertTrue("UC_4.h", containsGUILinkWith(0, 48, "tr", "tr.html", renderedObjects));
         //This page has 4 GUILinks and 5 GUIStrings
 
         //5 User navigates to a desired webpage. (using hyperlink)
@@ -59,7 +59,7 @@ public class UseCaseTest {
         }
         //6 Application shows the desired webpage.
         assertEquals("UC_4.c", window.getAddress(), "https://people.cs.kuleuven.be/~bart.jacobs/" + href);
-        assertTrue("UC_4.d", window.getDocArea().getDrawnGUIObjects().size() == 2);
+        assertTrue("UC_4.d", window.getDocArea().getDrawnGUIObjects().size() == 3);
         //error document has 2 GUIStrings
         for(GUIObject obj : window.getDocArea().getDrawnGUIObjects()){
             assertFalse("UC_4.e", obj instanceof GUILink);
