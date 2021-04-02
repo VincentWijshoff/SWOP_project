e676f569a41be309eb5ca5c8c02a7ef042c2fa64
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2021-04-02 13:15:00 +0200

    Beginnings off save html dialog screen

--- src/gui/DialogScreen/SaveHtmlScreen.java
+++ src/gui/DialogScreen/SaveHtmlScreen.java
@@ -3,19 +3,21 @@ package gui.DialogScreen;
 import gui.Screen;
 import gui.Window;
 
-import java.awt.*;
-
 public class SaveHtmlScreen extends DialogScreen{
     public SaveHtmlScreen(Window window, Screen prevScreen){
         super(window, prevScreen);
         this.create();
     }
 
+//    The user can save the current document as a .html file by pressing Ctrl+S.
+//    This shows the Save As dialog screen, with a text input field for the filename,
+//    preceded by a label “File name”, and two buttons: a “Save” button and a “Cancel”
+//    button. Clicking the “Save” button creates a file with the given name and writes
+//    the currently shown HTML document to it, and closes the dialog screen. Clicking
+//    the “Cancel” button simply closes the dialog screen.
+
     protected void create(){
         System.out.println("Creating save html screen");
     }
 
-    public void draw(Graphics g){
-
-    }
 }
