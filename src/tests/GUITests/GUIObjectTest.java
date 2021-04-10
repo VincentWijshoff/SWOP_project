024f7cebbe91f787289d6999a63b4c6b9dab8119
Toon Van Hove <toonvanhove@hotmail.com>
2021-04-10 11:01:26 +0200

    Add GUITableTest

--- src/tests/GUITests/GUIObjectTest.java
+++ src/tests/GUITests/GUIObjectTest.java
@@ -1,5 +1,6 @@
 package tests.GUITests;
-import gui.Objects.GUIButton;
+import events.EventHandler;
+import gui.Objects.*;
 import gui.Window;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.TestInstance;
 
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 import static tests.TestUtil.*;
 
@@ -38,5 +40,47 @@ public class GUIObjectTest {
         // by default teh color off the button should be Light gray
         assertTrue("ButtonTestInitializers", button2.buttonColor.equals(Color.lightGray));
         assertTrue("ButtonTestInitializers", button1.buttonColor.equals(Color.lightGray));
+    }
+
+    @Test
+    void GUITableTest() {
+        GUITable table1 = new GUITable(0,0);
+
+        GUITable nestedTable = new GUITable(0,0);
+        ArrayList<GUIObject> nestedRow = new ArrayList<>();
+        nestedRow.add(new GUIString("nested string"));
+        nestedTable.addRow(nestedRow);
+
+        ArrayList<GUIObject> row1 = new ArrayList<>();
+        row1.add(nestedTable);
+        row1.add(new GUIString("jaja"));
+        table1.addRow(row1);
+
+        ArrayList<GUIObject> row2 = new ArrayList<>();
+        row2.add(new GUILink("link", "href"));
+        table1.addRow(row2);
+
+        table1.setHandler((EventHandler) window.getCurrentScreen());
+        table1.updateDimensions();
+        System.out.println("jaja");
+
+        // nested table
+        assertEquals("1.a", row1.get(0).height, fm.getHeight());
+        assertEquals("1.b", row1.get(0).width, fm.stringWidth("nested string"));
+        assertEquals("1.c", row1.get(0).coordX, 0);
+        assertEquals("1.d", row1.get(0).coordY, 0);
+
+        // string
+        assertEquals("2.a", row1.get(1).height, fm.getHeight());
+        assertEquals("2.b", row1.get(1).width, fm.stringWidth("jaja"));
+        assertEquals("2.c", row1.get(1).coordX, fm.stringWidth("nested string") + GUITable.xMargin);
+        assertEquals("2.d", row1.get(1).coordY, 0);
+
+        // link
+        assertEquals("3.a", row2.get(0).height, fm.getHeight());
+        assertEquals("3.b", row2.get(0).width, fm.stringWidth("link"));
+        assertEquals("3.c", row2.get(0).coordX, 0);
+        assertEquals("3.d", row2.get(0).coordY, fm.getHeight());
+
     }
 }
