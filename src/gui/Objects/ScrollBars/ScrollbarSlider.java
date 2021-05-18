126a417b68db6f041d51fe523aabafd04523fb56
JoBoVa <jonas.vanmalcot@student.kuleuven.be>
2021-05-18 23:02:08 +0200

    update scrollbarSlider handle click sequence diagram (kinda scuffed) and update class diagram

--- src/gui/Objects/ScrollBars/ScrollbarSlider.java
+++ src/gui/Objects/ScrollBars/ScrollbarSlider.java
@@ -94,45 +94,62 @@ public class ScrollbarSlider {
         System.out.println("Clicked on scrollbar slider!");
         if (!this.canSlide) return;
 
-        // User clicked on the slider, change the slider color and save where the click happened in the slider
         if (id == MouseEvent.MOUSE_PRESSED) {
-            this.isSliding = true;
-            this.sliderColor = Color.LIGHT_GRAY;
-            this.cursorStart = this.isHorizontal ? x-coordX : y-coordY;
+            startClick(x, y);
+        } else if (id == MouseEvent.MOUSE_DRAGGED && this.isSliding) {
+            drag(x, y);
+        } else if (id == MouseEvent.MOUSE_RELEASED) {
+            releaseClick();
         }
+    }
 
-        // User is dragging the slider, change coordX
-        else if (id == MouseEvent.MOUSE_DRAGGED && this.isSliding) {
-            if (isHorizontal) {
-                int newSliderCoordX = x-this.cursorStart;
+    /**
+     * User clicked on the slider, change the slider color and save where the click happened in the slider
+     * @param x x-coordinate of the click
+     * @param y y-coordinate of the click
+     */
+    private void startClick(int x, int y){
+        this.isSliding = true;
+        this.sliderColor = Color.LIGHT_GRAY;
+        this.cursorStart = this.isHorizontal ? x-coordX : y-coordY;
+    }
 
-                // Keep the slider within scrollbar boundaries
-                if ((newSliderCoordX > this.scrollBar.getSliderStart()) && (newSliderCoordX+this.width < this.scrollBar.getSliderEnd())) {
-                    this.coordX = newSliderCoordX; // Slide with mouse
-                } else if (newSliderCoordX <= this.scrollBar.getSliderStart()) {
-                    this.coordX = this.scrollBar.getSliderStart(); // Slide most left
-                } else {
-                    this.coordX = this.scrollBar.getSliderEnd()-this.width; // Slide most right
-                }
+    /**
+     * User is dragging the slider, change coordX (horizontal slider) or coordY (vertical slider)
+     * @param x the x-coordinate of the slided click
+     * @param y the y-coordinate of the slided click
+     */
+    private void drag(int x, int y){
+        if (isHorizontal) {
+            int newSliderCoordX = x-this.cursorStart;
+
+            // Keep the slider within scrollbar boundaries
+            if ((newSliderCoordX > this.scrollBar.getSliderStart()) && (newSliderCoordX+this.width < this.scrollBar.getSliderEnd())) {
+                this.coordX = newSliderCoordX; // Slide with mouse
+            } else if (newSliderCoordX <= this.scrollBar.getSliderStart()) {
+                this.coordX = this.scrollBar.getSliderStart(); // Slide most left
             } else {
-                int newSliderCoordY = y-this.cursorStart;
-                // Keep the slider within scrollbar boundaries
-                if ((newSliderCoordY > this.scrollBar.getSliderStart())/* && (newSliderCoordY+this.height < this.scrollBar.getSliderEnd())*/) {
-                    this.coordY = newSliderCoordY; // Slide with mouse
-                } else if (newSliderCoordY <= this.scrollBar.getSliderStart()) {
-                    this.coordY = this.scrollBar.getSliderStart(); // Slide most up
-                } else {
-                    this.coordY = this.scrollBar.getSliderEnd()-this.height; // Slide most down
-                }
+                this.coordX = this.scrollBar.getSliderEnd()-this.width; // Slide most right
             }
-
+        } else {
+            int newSliderCoordY = y-this.cursorStart;
+            // Keep the slider within scrollbar boundaries
+            if ((newSliderCoordY > this.scrollBar.getSliderStart())/* && (newSliderCoordY+this.height < this.scrollBar.getSliderEnd())*/) {
+                this.coordY = newSliderCoordY; // Slide with mouse
+            } else if (newSliderCoordY <= this.scrollBar.getSliderStart()) {
+                this.coordY = this.scrollBar.getSliderStart(); // Slide most up
+            } else {
+                this.coordY = this.scrollBar.getSliderEnd()-this.height; // Slide most down
+            }
         }
+    }
 
-        // User stops dragging
-        else if (id == MouseEvent.MOUSE_RELEASED) {
-            this.isSliding = false;
-            this.sliderColor = Color.WHITE;
-        }
+    /**
+     * User stops dragging
+     */
+    private void releaseClick(){
+        this.isSliding = false;
+        this.sliderColor = Color.WHITE;
     }
 
 }
